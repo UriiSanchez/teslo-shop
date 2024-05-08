7677d811229aaa199785a0778366ae3396c0ca3a
Uriel Antonio Sánchez <uriel.antonio.sanchez@gmail.com>
2024-05-08 14:21:17 -0600

    Dev Cerón | add: components not found, top menu and title

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,5 +1,17 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
-export default function () {
+interface Props {
+	params: {
+		id: string;
+	};
+}
+export default function ({ params }: Props) {
+	const { id } = params;
+
+	if (id === "kid") {
+		notFound();
+	}
+
 	return <div>Category Page</div>;
 }
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,9 @@
-import { titleFont } from "@/config/fonts";
+import { Title } from "@/components";
 
 export default function Home() {
 	return (
-		<div className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<h1>Hola mundo</h1>
-			<h1 className={titleFont.className}>Hola mundo</h1>
-		</div>
+		<>
+			<Title title='Tienda' subtitle='Todos los productos' className='mb-2' />
+		</>
 	);
 }
