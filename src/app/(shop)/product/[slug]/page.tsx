8dd580a6444a771de11f36f4b46d8f15adc9dad6
Uriel Antonio Sánchez <uriel.antonio.sanchez@gmail.com>
2024-07-16 16:25:11 -0600

    Dev Cerón | update: improvements added for metatags and slug searching

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,13 +1,16 @@
+export const revalidate = 604800;
 import { notFound } from "next/navigation";
 
-import { initialData } from "@/seed/seed";
 import { titleFont } from "@/config/fonts";
 import {
 	ProductMobileSlideShow,
 	ProductSlideShow,
 	QuantitySelector,
 	SizeSelector,
+	StockLabel,
 } from "@/components";
+import { getProductBySlug } from "@/actions";
+import { Metadata, ResolvingMetadata } from "next";
 
 interface Props {
 	params: {
@@ -15,9 +18,34 @@ interface Props {
 	};
 }
 
-export default function ProductBySlugPage({ params }: Props) {
+export async function generateMetadata(
+	{ params }: Props,
+	parent: ResolvingMetadata
+): Promise<Metadata> {
+	// read route params
+	const slug = params.slug;
+
+	// fetch data
+	const product = await getProductBySlug(slug);
+
+	// optionally access and extend (rather than replace) parent metadata
+	const previousImages = (await parent).openGraph?.images || [];
+
+	return {
+		title: product?.title ?? "Producto no encontrado",
+		description: product?.description ?? "",
+		openGraph: {
+			title: product?.title ?? "Producto no encontrado",
+			description: product?.description ?? "",
+			images: [`/products/${product?.images[1]}`],
+		},
+	};
+}
+
+export default async function ProductBySlugPage({ params }: Props) {
 	const { slug } = params;
-	const product = initialData.products.find((prod) => prod.slug === slug);
+	const product = await getProductBySlug(slug);
+
 	if (!product) {
 		notFound();
 	}
@@ -41,6 +69,7 @@ export default function ProductBySlugPage({ params }: Props) {
 			</div>
 			{/* Detalles */}
 			<div className='col-span-1 px-5'>
+				<StockLabel slug={product.slug} />
 				<h1 className={`${titleFont.className} antialiased font-bold text-xl`}>
 					{product.title}
 				</h1>
